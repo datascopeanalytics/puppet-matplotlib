5e1237be4a7abe07e6e93dfe629878fb76197275
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-08-17 15:22:38 -0500

    cleaned up params.pp to use a case statement instead

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,34 +1,22 @@
 class matplotlib::params {
-  $_os_failure = "operating stystem ${operatingsystem} not yet supported"
   
   # use pip by default to get the latest and greatest version
   $provider = "pip"
 
-  $numpy = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/ => 'python-numpy',
-    default                 => fail($_os_failure),
-  }
-
-  # whatever package provides freetype2 + freetype-config command
-  $freetype = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/ => 'libfreetype6-dev',
-    default                 => fail($_os_failure),
-  }
-  
-  $png = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/ => 'libpng12-dev',
-    default                 => fail($_os_failure),
-  }
-
-  # latex package name for particular system
+  # by default, do not install latex as it takes quite a long time
   $install_latex = false
-  $latex = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/ => 'texlive',
-    default                 => fail($_os_failure),
-  }
-  $dvipng = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/ => 'dvipng',
-    default                 => fail($_os_failure),
-  }
   
+  case $operatingsystem {
+    /(?i-mx:ubuntu|debian)/:   {
+      $numpy    = 'python-numpy'
+      $freetype = 'libfreetype6-dev'
+      $png      = 'libpng12-dev'
+      $latex    = 'texlive'
+      $dvipng   = 'dvipng'
+    }
+    default: {
+      fail("operating stystem ${operatingsystem} not yet supported")
+    }
+  }
+
 }
